1637c63bb29131e9f5c47a392e974cffc26e0bf7
Brandon Service <brservice@comcast.net>
2020-11-09 16:57:10 -0500

    A05 Deliverable

--- IGME309-2201-master/A05 - Octree/AppClass.cpp
+++ IGME309-2201-master/A05 - Octree/AppClass.cpp
@@ -56,7 +56,14 @@ void Application::Display(void)
 	ClearScreen();
 
 	//display octree
-	m_pRoot->Display();
+	if (m_uOctantID == -1)
+	{
+		m_pRoot->Display();
+	}
+	else
+	{
+		m_pRoot->Display(m_uOctantID);
+	}
 	
 	// draw a skybox
 	m_pMeshMngr->AddSkyboxToRenderList();
